f9dea6d9a0606a503e2ca60453bc1fbd40c20e7b
Nikki-SU <hayek_1226@163.com>
2026-05-24 15:15:01 +0800

    feat: 笔记页面 - 更新 frontend/src/router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -7,6 +7,7 @@ import Tracking from './pages/Tracking'
 import Browse from './pages/Browse'
 import DeepRead from './pages/DeepRead'
 import Manage from './pages/Manage'
+import Notes from './pages/Notes'
 import Learn from './pages/Learn'
 import Settings from './pages/Settings'
 
@@ -19,6 +20,7 @@ function AppRouter() {
           <Route path="browse" element={<Browse />} />
           <Route path="deep-read" element={<DeepRead />} />
           <Route path="manage" element={<Manage />} />
+          <Route path="notes" element={<Notes />} />
           <Route path="learn" element={<Learn />} />
           <Route path="settings" element={<Settings />} />
         </Route>
